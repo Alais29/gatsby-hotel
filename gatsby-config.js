10e989115c662a10ae77c17606ee0a85a368b50f
Alfonsina <alfonsina.l@gmail.com>
2020-12-12 14:56:57 -0300

    feature: Add data from DatoCms

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `8f58fd8ff5e3b3c5881c4b5fc903e9`
+      }
+    }
   ],
 }
--- src/pages/nosotros.jsx
+++ src/pages/nosotros.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import ContenidoNosotros from "../components/ContenidoNosotros"
 
 const Nosotros = () => (
   <Layout>
-    <h1>Nosotros</h1>
+    <ContenidoNosotros />
   </Layout>
 )
 
